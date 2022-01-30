f6b382fc981baf7b015e3c58943ad60259f20d2d
Raphael <raphaelcamar@outlook.com>
2022-01-30 16:03:12 -0300

    test: ensure login starts with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,9 +3,12 @@ import { render } from '@testing-library/react'
 import Login from './index'
 
 describe('first', () => {
-  test('Should not render spinner and error on start', () => {
+  test('Shouldstart with initial state', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
   })
 })
