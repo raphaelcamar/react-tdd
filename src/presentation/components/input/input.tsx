0a576d6edc3078e26c6c67fbed14e1730e5084b9
Raphael <raphaelcamar@outlook.com>
2022-02-05 10:57:05 -0300

    test: ensure Login present valid state if password is invalid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -20,11 +20,11 @@ const Input: React.FC<Props> = (props) => {
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo certo'
   }
 
   return (
