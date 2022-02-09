5d4cfbd8d0ba77066f884b184a5d1e3073af6e05
Raphael <raphaelcamar@outlook.com>
2022-02-09 20:59:28 -0300

    feat: ensure Login present error and hide spinner on authentication failure

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -30,9 +30,13 @@ const Login: React.FC<Props> = ({ validation, authentication }) => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
-    if (state.isLoading || state.emailError || state.passwordError) return
-    setState({ ...state, isLoading: true })
-    await authentication.auth({ email: state.email, password: state.password })
+    try {
+      if (state.isLoading || state.emailError || state.passwordError) return
+      setState({ ...state, isLoading: true })
+      await authentication.auth({ email: state.email, password: state.password })
+    } catch (error) {
+      setState({ ...state, isLoading: false, mainError: error.message })
+    }
   }
 
   return (
--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
+import { render, RenderResult, fireEvent, cleanup, waitFor } from '@testing-library/react'
 import Login from './index'
 import { ValidationStub } from '@/presentation/test/mock-validation'
 import faker from 'faker'
 import { AuthenticationSpy } from '@/presentation/test'
+import { InvalidCredentialsError } from '@/domain/errors'
 
 type SutTypes = {
   sut: RenderResult
@@ -152,5 +153,17 @@ describe('Login component', () => {
     const { sut, authenticationSpy } = makeSut({ validationError })
     fireEvent.submit(sut.getByTestId('form'))
     expect(authenticationSpy.callsCount).toBe(0)
+  })
+
+  test('Should present error if auth fails', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    const error = new InvalidCredentialsError()
+    jest.spyOn(authenticationSpy, 'auth').mockReturnValueOnce(Promise.reject(error))
+    simulateValidSubmit(sut)
+    const errorWrap = sut.getByTestId('error-wrap')
+    await waitFor(() => errorWrap)
+    const mainError = sut.getByTestId('main-error')
+    expect(mainError.textContent).toBe(error.message)
+    expect(errorWrap.childElementCount).toBe(1)
   })
 })
