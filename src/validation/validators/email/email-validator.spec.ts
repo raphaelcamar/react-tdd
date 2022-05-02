e73d62a639196f96db83a456c191baa8724bf377
Raphael <raphaelcamar@outlook.com>
2022-05-02 19:00:06 -0300

    feat: ensure e-mail validation returns falsy if email is valid

--- src/validation/validators/email/email-validator.spec.ts
+++ src/validation/validators/email/email-validator.spec.ts
@@ -1,12 +1,21 @@
 import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 import { EmailValidation } from './email-validation'
+import faker from 'faker'
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
+    const sut = new EmailValidation(faker.random.word())
 
-    const error = sut.validate('')
+    const error = sut.validate(faker.random.word())
 
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.random.word())
+
+    const error = sut.validate(faker.internet.email())
+
+    expect(error).toBeFalsy()
   })
 })
