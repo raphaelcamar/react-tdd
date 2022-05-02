18b20cd0a13405a36a9d07af9d4069169da2fcc6
Raphael <raphaelcamar@outlook.com>
2022-05-02 19:10:34 -0300

    feat: ensure MinLengthValidation returns falsy if value is valid

--- src/validation/validators/min-lengh/min-length.spec.ts
+++ src/validation/validators/min-lengh/min-length.spec.ts
@@ -2,11 +2,19 @@ import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 import { MinLengthValidation } from './min-length'
 
 describe('MinLengthValidation', () => {
-  test('Should return error i value is invalid', () => {
+  test('Should return error if value is invalid', () => {
     const sut = new MinLengthValidation('field', 5)
 
     const error = sut.validate('123')
 
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if value is valid', () => {
+    const sut = new MinLengthValidation('field', 5)
+
+    const error = sut.validate('12345')
+
+    expect(error).toBeFalsy()
   })
 })
--- src/validation/validators/min-lengh/min-length.ts
+++ src/validation/validators/min-lengh/min-length.ts
@@ -2,9 +2,9 @@ import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 import { FieldValidation } from '@/validation/protocols'
 
 export class MinLengthValidation implements FieldValidation {
-  constructor (readonly field: string, private readonly minLenth: number) {}
+  constructor (readonly field: string, private readonly minLength: number) {}
 
   validate (value: string): Error {
-    return new InvalidFieldError()
+    return value.length >= this.minLength ? null : new InvalidFieldError()
   }
 }
