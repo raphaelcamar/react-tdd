42d8a8a70b0fd68bd23afe1cb094a8bcd0a470ce
Raphael <raphaelcamar@outlook.com>
2022-05-02 19:01:28 -0300

    refactor: move sut SUT creation to a factory

--- src/validation/validators/email/email-validator.spec.ts
+++ src/validation/validators/email/email-validator.spec.ts
@@ -2,9 +2,11 @@ import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 import { EmailValidation } from './email-validation'
 import faker from 'faker'
 
+const makeSut = (): EmailValidation => new EmailValidation(faker.database.column())
+
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValidation(faker.random.word())
+    const sut = makeSut()
 
     const error = sut.validate(faker.random.word())
 
@@ -12,7 +14,7 @@ describe('EmailValidation', () => {
   })
 
   test('Should return falsy if email is valid', () => {
-    const sut = new EmailValidation(faker.random.word())
+    const sut = makeSut()
 
     const error = sut.validate(faker.internet.email())
 
