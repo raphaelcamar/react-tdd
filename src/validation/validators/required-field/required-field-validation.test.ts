2d3323ded167ae80a1820ba149196cda29953f4c
Raphael <raphaelcamar@outlook.com>
2022-05-02 18:47:36 -0300

    refactor: move field validation to a differente folder

--- src/validation/validators/required-field/required-field-validation.test.ts
+++ src/validation/validators/required-field/required-field-validation.test.ts
@@ -1,4 +1,4 @@
-import { RequiredFieldError } from '../errors/required-field-error'
+import { RequiredFieldError } from '@/validation/errors'
 import { RequiredFieldValidation } from './required-field-validation'
 import faker from 'faker'
 
