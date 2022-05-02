a8604f022e7db961b400d8baeb88b08ee4c53d1c
Raphael <raphaelcamar@outlook.com>
2022-05-02 18:55:58 -0300

    feat: ensure e-mail validation returns error ir email is invalid

---
+++ src/validation/validators/email/email-validator.spec.ts
@@ -0,0 +1,12 @@
+import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
+import { EmailValidation } from './email-validation'
+
+describe('EmailValidation', () => {
+  test('Should return error if email is invalid', () => {
+    const sut = new EmailValidation('email')
+
+    const error = sut.validate('')
+
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
