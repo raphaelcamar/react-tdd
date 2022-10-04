80bdedec652f78ec830a3afb330354202fa91c2a
Raphael <raphaelcamar@outlook.com>
2022-10-04 11:58:54 -0300

    refactor: fieldname on makeSut

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -3,24 +3,24 @@ import { fireEvent, render, RenderResult } from '@testing-library/react'
 import Input from './input'
 import Context from '@/presentation/contexts/form/form-context'
 
-const makeSut = (): RenderResult => {
+const makeSut = (fieldname: string): RenderResult => {
   return render(
     <Context.Provider value={{ state: {} }}>
-      <Input name="field" />
+      <Input name={fieldname} />
     </Context.Provider>
   )
 }
 
 describe('Input Component', () => {
   test('should begin with readOnly', () => {
-    const sut = makeSut()
+    const sut = makeSut('field')
     const input = sut.getByTestId('field') as HTMLInputElement
 
     expect(input.readOnly).toBe(true)
   })
 
   test('should remove readOnly on focus', () => {
-    const sut = makeSut()
+    const sut = makeSut('field')
     const input = sut.getByTestId('field') as HTMLInputElement
     fireEvent.focus(input)
     expect(input.readOnly).toBe(false)
